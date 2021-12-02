b93cfa8a589f9c48eb1f725b25dd8559f6d4ed6a
Nick Murphy <nmurphy@neworbit.co.uk>
2021-12-02 11:21:40 +0000

    setup for day 2

---
+++ src/day2.rs
@@ -0,0 +1,13 @@
+use crate::extensions::import_file;
+
+pub fn day2() -> i32 {
+    return 2;
+}
+
+#[cfg(test)]
+mod day2_tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod extensions;
 mod day1;
+mod day2;
 use day1::day1_2;
 
 fn main() {
